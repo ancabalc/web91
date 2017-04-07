f22ec77114c15c095d4cfd9bafc94a854cf5365d
siitwebcluj <paul.brie@scoalainformala.ro>
2017-04-07 18:53:07 +0000

    config file and DB connection

--- api/models/DB.php
+++ api/models/DB.php
@@ -1,7 +1,18 @@
 <?php
 
-class DB 
-{
+class DB {
+    protected $dbh;
     
-};
+    function __construct() {
+
+        try {
+         $this->dbh = new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, 
+                          DBUSER, 
+                          DBPASS);
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
 
