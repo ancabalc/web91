d55f2bb47b473ebb8c46dd953ae51b696577a0d1
Rares Ienciu <rares_ienciu@yahoo.com>
2017-04-10 17:49:07 +0000

    Implement Create Application

--- api/configs/routes.php
+++ api/configs/routes.php
@@ -1,3 +1,4 @@
 <?php
     $routes['/api/users/update'] = array( "class" => "", "method" => "");
+    $routes['/api/aplication/add'] = array("class"=>"Applications", "method"=>"createItem");
 ?>
--- api/models/ApplicationsModel.php
+++ api/models/ApplicationsModel.php
@@ -3,8 +3,16 @@
    require_once "DB.php";
    
    class ApplicationsModel extends DB {
-        function insertItem() {
-       
-           
-        }
+      
+        function insertItem($item) {
+        $sql = 'insert into applications (title, description, active) values(?,?,?)';
+
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute(array($item['title'], 
+                            $item['description'], 
+                            $item['active']));
+        
+        return $this->dbh->lastInsertId();
+    }
+    
    }
