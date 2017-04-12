39f8cee4b1469920f620fa136c85e72d503600c3
ciprian <simionov.ciprian@gmail.com>
2017-04-12 16:19:54 +0000

    routes update

--- api/configs/routes.php
+++ api/configs/routes.php
@@ -2,5 +2,4 @@
     $routes['/accounts/login'] = array("class"=>"Accounts", "method"=>"login");
     $routes['/users/update'] = array("class" => "Users", "method" => "updateUser");
     $routes['/accounts/create'] = array( "class" => "Accounts", "method" => "createAccount");
-
 ?>
