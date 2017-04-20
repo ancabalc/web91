b6cc8e4737e715690299162f4d228ce6bd58e30c
ciprian <simionov.ciprian@gmail.com>
2017-04-20 15:57:11 +0000

    ciprian_update routes.php

--- api/configs/routes.php
+++ api/configs/routes.php
@@ -5,6 +5,7 @@
     $routes['/aplication/add'] = array("class"=>"Applications", "method"=>"createItem"); 
     $routes['/accounts/login'] = array("class"=>"Accounts", "method"=>"login");
     $routes['/users/update'] = array("class" => "Users", "method" => "updateUser");
+     $routes['/users/list'] = array ("class"=> "Users", "method" => "listTopProviders");
     $routes['/accounts/create'] = array( "class" => "Accounts", "method" => "createAccount");
 
 ?>
